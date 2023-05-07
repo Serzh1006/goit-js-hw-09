5c42b11aef5b49fc0252b9b36e94b74bfab7fa0c
Serzh1006 <serzh.kravchenko.9696@mail.ru>
2023-05-07 18:31:47 +0200

    task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const promiseObj = {};
+      const shouldResolve = Math.random() > 0.3;
+      promiseObj.position = position;
+      promiseObj.delay = delay;
+      if (shouldResolve) {
+        resolve(promiseObj);
+      } else {
+        reject(promiseObj);
+      }
+    }, delay);
+  });
+}
+
+const fomrEl = document.querySelector('.form');
+const objData = {};
+
+fomrEl.addEventListener('submit', onClickGetData, { once: true });
+
+function onClickGetData(e) {
+  e.preventDefault();
+  const formData = new FormData(fomrEl);
+  formData.forEach((value, name) => {
+    objData[name] = Number(value);
+  });
+
+  for (let i = 1; i <= objData.amount; i += 1) {
+    createPromise(i, objData.delay)
+      .then(({ position, delay }) =>
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`)
+      );
+    objData.delay += objData.step;
   }
 }
